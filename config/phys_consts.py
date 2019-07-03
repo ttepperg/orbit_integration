7aff7932fc0730e1773df2449471b4c3f645d93e
Thor Tepper-Garcia <mail@thorsten.mx>
2019-07-03 20:50:40 +1000

    Small edits to config/phys_consts.py

--- config/phys_consts.py
+++ config/phys_consts.py
@@ -1,14 +1,17 @@
+'''
+Author:	Thorsten Tepper Garcia
+Date:	03/07/2019
+'''
+# from astropy import constants
+# 
 # Interesting note:
 # The use of Grav obtained from astropy and normalised as follows
 # leads to a significant increase in the integration time, presumably
 # because Grav carries a lot of structure with (it is a Quantity object)
 # it and the function that depends on it is called several 1000 times in a loop.
 # 
-# from astropy import constants
-# 
 # # Gravitational constant (in astrophysical units):
 # myUnit = units.Unit("kpc km^2 / Msun s^2")
-# # myGrav = G.to("kpc^3 / Msun s^2")
 # myGrav = G.to(myUnit)
 # 
 # # Make it dimensionless (but recall that it is still a Quantity object):
@@ -20,4 +23,4 @@
 Grav = 4.300917270069976e-06	# units: kpc km^2 / Msun s^2
 
 # Note that the adopted units fix the units of mass, length, and
-# velocity; the last two fix the unit of time.
+# velocity; the last two fix the unit of time. See units.py.
