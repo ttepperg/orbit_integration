a524f6a7c2f196ae7730353b00ed0d010ea587b5
Thor Tepper-Garcia <mail@thorsten.mx>
2019-07-12 15:47:15 +1000

    Moved initial conditons setup to utils/io.py

--- two_body/two_body_orbit.py
+++ two_body/two_body_orbit.py
@@ -11,80 +11,25 @@ See README for information on the code's background, usage, etc.
 # Import necessary modules
 
 # built-in modules
-import importlib									# needed to import ICs' as module
 import sys
 
 # costum modules
 sys.path.insert(0,"../.")							# include top directory in module search path
-sys.path.insert(0,"./init")							# include initial conditions directory
 from utils import io
-from class_defs import body
 from class_defs import orbit
 
 
-# Collect input argument
-initialConds = io.get_input()
-
-# Set initial conditions
-# (consider moving this into a function or class)
-ic = importlib.import_module(initialConds)
-print("\nGathering input parameters from file {}...".format(initialConds))
-
-# Time integration settings
-try:
-	t0 = ic.t_0
-except:
-	t0 = 0.
-try:
-	t1 = ic.t_1
-except:
-	t1 = 10.
-try:
-	timeStep = ic.delta_t
-except:
-	timeStep = 0.001
-
-# Body 1
-try:
-	Pot1 = ic.Potential1
-except:
-	Pot1 = None
-try:
-	DF1 = ic.Dynamical_Friction1
-except:
-	DF1 = None
-r10_vec = [ic.x1_0,ic.y1_0,ic.z1_0]
-v10_vec = [ic.vx1_0,ic.vy1_0,ic.vz1_0]
-body1 = body.Body(mass=ic.Mass1,pot=Pot1,r_vec=r10_vec,v_vec=v10_vec,df=DF1)
-
-# Body 2
-try:
-	Pot2 = ic.Potential2
-except:
-	Pot2 = None
-try:
-	DF2 = ic.Dynamical_Friction2
-except:
-	DF2 = None
-r20_vec = [ic.x2_0,ic.y2_0,ic.z2_0]
-v20_vec = [ic.vx2_0,ic.vy2_0,ic.vz2_0]
-body2 = body.Body(mass=ic.Mass2,pot=Pot2,r_vec=r20_vec,v_vec=v20_vec,df=DF2)
-
-# sanity check
-if DF1 is not None and DF2 is not None:
-	raise ValueError("Both bodies cannot excert dynamical friction simultaneously")
-else:
-	print("Done.")
-
+# Collect input argument and set initial conditions
+ics = io.get_input()
 
 # Initialise Orbit object
-orbit = orbit.Orbit(body1,body2)
+orbit = orbit.Orbit(ics.body1,ics.body2)
 
 # Integrate orbit
-time, EoM = orbit.integrate(t0,t1,timeStep)
+time, EoM = orbit.integrate(ics.t_0,ics.t_1,ics.delta_t)
 
 # Write orbit evolution to file
 outDir = "./output/"
-outFile = outDir + initialConds + "_out.dat"
+outFile = outDir + ics.filename_prefix + "_out.dat"
 
 orbit.write_table(time_list = time, state_vector = EoM, filename = outFile, output_freq = 10)
--- utils/io.py
+++ utils/io.py
@@ -3,29 +3,76 @@ Author:	Thorsten Tepper Garcia
 Date:	02/07/2019
 '''
 
+import sys
+sys.path.insert(0,"./init")							# include initial conditions directory
+import importlib									# needed to import ICs' as module
+from class_defs import body
+
 def get_input():
-	import sys
 	if len(sys.argv) < 2:
 		print("\nUSAGE:")
 		print("{} <input parameter file>\n".format(sys.argv[0]))
 		exit()
 	else:
+		relative_path='../two_body/init'
 		ics_file = sys.argv[1]
 		ext = ".py"
 		if ext in ics_file:
 			print("\nRemoved unnecessary extension {} in input parameter file.".format(ext))
-			return ics_file.replace(ext,'') 	# remove file extension if present
+			ics_file = ics_file.replace(ext,'') 	# remove file extension if present
+
+		# Set initial conditions
+		print("\nGathering input parameters from file {}...".format(ics_file))
+		ic = importlib.import_module(ics_file,package=relative_path)
+
+		# Body 1
+		try:
+			Pot1 = ic.Potential1
+		except:
+			Pot1 = None
+		try:
+			DF1 = ic.Dynamical_Friction1
+		except:
+			DF1 = None
+		r10_vec = [ic.x1_0,ic.y1_0,ic.z1_0]
+		v10_vec = [ic.vx1_0,ic.vy1_0,ic.vz1_0]
+		ic.body1 = body.Body(mass=ic.Mass1,pot=Pot1,r_vec=r10_vec,v_vec=v10_vec,df=DF1)
+
+		# Body 2
+		try:
+			Pot2 = ic.Potential2
+		except:
+			Pot2 = None
+		try:
+			DF2 = ic.Dynamical_Friction2
+		except:
+			DF2 = None
+		r20_vec = [ic.x2_0,ic.y2_0,ic.z2_0]
+		v20_vec = [ic.vx2_0,ic.vy2_0,ic.vz2_0]
+		ic.body2 = body.Body(mass=ic.Mass2,pot=Pot2,r_vec=r20_vec,v_vec=v20_vec,df=DF2)
+
+		# sanity check
+		if DF1 is not None and DF2 is not None:
+			raise ValueError("Both bodies cannot excert dynamical friction simultaneously")
 		else:
-			return ics_file
+			print("Done.")
 
+		# Time integration settings
+		try:
+			ic.t_0
+		except:
+			ic.t_0 = 0.
+		try:
+			ic.t_1
+		except:
+			ic.t_1 = 10.
+		try:
+			ic.delta_t
+		except:
+			ic.delta_t = 0.001
 
-# def write_table(file = None):
-# 	'''dumps code results to ascii file'''
-# 	if file is None:
-# 		raise ValueError("output file name not provided in write_table")
-# 	else:
-# 		f = open(file, 'wt')
-# 		f.write("test\n")
-# 		f.close()
+		# save init file name
+		ic.filename_prefix = ics_file
 
-	
+		return ic
+
