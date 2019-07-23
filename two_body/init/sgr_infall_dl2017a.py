1ae8a34c7f8bbe2cc414333918beb94a9b249c9e
Thor Tepper-Garcia <mail@thorsten.mx>
2019-07-23 20:17:38 +1000

    Small edits to two_body/init/sgr_infall_dl2017a.py

--- two_body/init/sgr_infall_dl2017a.py
+++ two_body/init/sgr_infall_dl2017a.py
@@ -23,8 +23,8 @@ t_1 = 7.85														# total time (time unit ~ 0.978 Gyr)
 delta_t = 1.0e-3												# integration time step
 
 # Milky Way
-Mass1 = 1.25e12 #1.325e12												# total mass (Msun)
-a1 = 25. #38.35														# scale radius (kpc)
+Mass1 = 1.25e12 #1.325e12										# total mass (Msun)
+a1 = 25. #38.35													# scale radius (kpc)
 Potential1 = funcs.Hernquist_Potential(mass=Mass1,a=a1)			# potential (km/s)^2
 x1_0 = 0.														# positions (kpc)
 y1_0 = 0.
@@ -46,7 +46,7 @@ vz2_0 = 71.66
 
 
 # Dynamical friction settings
-soft_length2 = 0.3 #1.0													# softening length of Sgr
+soft_length2 = 0.3 #1.0												# softening length of Sgr
 Dynamical_Friction1 = funcs.dyn_friction_maxwell(eps=soft_length2)	# dynamical friction function
 Mass1_cum = funcs.Hernquist_Mass(mass=Mass1,a=a1)
 Mass2_cum = funcs.Hernquist_Mass(mass=Mass2,a=a2)
